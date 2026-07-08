6b337a9f169b9a6c2f3fde34532c0f75b244e7e8
Bhupendra Sah <080bct023@ioepc.edu.np>
2026-07-08 13:42:55 +0545

    fix cors issue again

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ initSocket(io);
 // server.js मा:
 app.use(cors({
   origin: [
-    "https://food-delivery-frontend-xi-blond.vercel.app", 
+    process.env.FRONTEND_URL,
     "http://localhost:5173"
   ],
   credentials: true,
